f06eef4bec41ffd5d6bf09a7202344eff8c9b0f9
Shadrack K Kimutai <shadrackkimutai@gmail.com>
2023-05-31 04:56:24 +0300

    Moved to Laravel
    Move to Laravel

--- m-vote/db-util.php
+++
@@ -1,9 +0,0 @@
-
-<?php
- define('HOST','mysql.hostinger.in');
- define('USER','u502452270_andro');
- define('PASS','belal_123');
- define('DB','u502452270_andro');
- 
- $con = mysqli_connect(HOST,USER,PASS,DB) or die('Unable to Connect');
- ?>
